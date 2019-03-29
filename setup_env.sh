8f2a2cf6e36d74020e5d09785bd156776a77231f
zsminor <zminor@stu.norwich.edu>
2019-03-29 15:58:26 -0400

    Create setup_env.sh

---
+++ setup_env.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+#installs dependencies for tensor flow docker images
+
+#run as root, or add sudo before every apt-* command
+
+#updates repos and updates installed packages. Installs docker. Installs tensorflow docker images.
+
+apt-get update && apt-get upgrade -y;
+apt-get install -y apt-transport-https ca-certificates curl gnupg2 software-properties-common;
+curl -fsSL https://download.docker.com/linux/debian/gpg | apt-key add -;
+add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/debian $(lsb_release -cs) stable";
+apt-get update -y;
+apt-get install -y docker-ce docker-ce-cli containerd.io;
+docker pull tensorflow/tensorflow:latest-gpu-jupyter;
+echo "dependencies installed!";
