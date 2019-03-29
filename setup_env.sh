571b8a9de723bcd601eee455f55f349313a59872
zsminor <zminor@stu.norwich.edu>
2019-03-29 16:02:38 -0400

    Update setup_env.sh

--- setup_env.sh
+++ setup_env.sh
@@ -1,16 +1,14 @@
 #!/bin/bash
 
-#installs dependencies for tensor flow docker images
-
-#run as root, or add sudo before every apt-* command
+#installs dependencies for tensor flow docker images, creates tensorflow-container and spawn a bash shell for it.
 
 #updates repos and updates installed packages. Installs docker. Installs tensorflow docker images.
 
-apt-get update && apt-get upgrade -y;
-apt-get install -y apt-transport-https ca-certificates curl gnupg2 software-properties-common;
-curl -fsSL https://download.docker.com/linux/debian/gpg | apt-key add -;
-add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/debian $(lsb_release -cs) stable";
-apt-get update -y;
-apt-get install -y docker-ce docker-ce-cli containerd.io;
+sudo apt-get update && apt-get upgrade -y;
+sudo apt-get install -y apt-transport-https ca-certificates curl gnupg2 software-properties-common;
+curl -fsSL https://download.docker.com/linux/debian/gpg | sudo apt-key add -;
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/debian $(lsb_release -cs) stable";
+sudo apt-get update -y;
+sudo apt-get install -y docker-ce docker-ce-cli containerd.io;
 docker pull tensorflow/tensorflow:latest-gpu-jupyter;
-echo "dependencies installed!";
+docker -it -P tensorflow/tensorflow:latest-gpu-jupyter bash;
